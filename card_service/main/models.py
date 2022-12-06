ba03ee35836e58edcff615a0ff4613fe064bb237
acclrtrrr <andrey.novikov2001@yandex.ru>
2022-12-07 00:36:57 +0300

    Add DataBase Models: Card, ShoppingHistory

--- card_service/main/models.py
+++ card_service/main/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
-# серия карты, номер карты, дата выпуска карты, дата окончания активности карты, дата использования, сумма, статус карты (не активирована/активирована/просрочена).
+
+class Card(models.Model):
+    CARD_STATUSES = [
+        ('Not activated', 'не активирована'),
+        ('Activated', 'активирована'),
+        ('Overdue', 'просрочена')
+    ]
+
+    series = models.PositiveIntegerField(
+        verbose_name='Серия',
+        max_length=16,
+        help_text='Введите серию карты'
+    )
+    number = models.PositiveIntegerField(
+        verbose_name='Номер',
+        max_length=16,
+        help_text='Введите номер карты'
+    )
+    release_date = models.DateTimeField(
+        verbose_name='Дата выпуска',
+        auto_now_add=True
+    )
+    activity_end_date = models.DateTimeField(
+        verbose_name='Дата окончания активности',
+    )
+    date_of_use = models.DateTimeField(
+        verbose_name='Дата использования',
+        auto_now_add=True
+    )
+    amount = models.DecimalField(
+        verbose_name='Сумма',
+        max_digits=11,
+        decimal_places=2,
+        default=0.00
+    )
+    status = models.CharField(
+        verbose_name='Статус',
+        choices=CARD_STATUSES,
+        max_length=50,
+        default='Not activated'
+    )
+
+    class Meta:
+        verbose_name = 'Карта'
+        verbose_name_plural = 'Карты'
+        ordering = ['-release_date']
+
+    def __str__(self):
+        return f'Card #{self.pk}'
+
+
+class ShoppingHistory(models.Model):
+    card = models.ForeignKey(
+        verbose_name='Карта',
+        to=Card,
+        on_delete=models.CASCADE,
+        related_name='history'
+    )
+    amount = models.DecimalField(
+        verbose_name='Сумма',
+        max_digits=11,
+        decimal_places=2,
+    )
+    buying_date = models.DateTimeField(
+        verbose_name='Дата покупки',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'История покупок карты'
+        verbose_name_plural = 'История покупок карт'
+        ordering = ['-buying_date']
+
+    def __str__(self):
+        return f'{self.card} history: {self.buying_date}'
